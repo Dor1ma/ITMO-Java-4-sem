49808685ac15d90fc08fdc798aa4f30f910c9bc3
Pavel <admaikinpavel@yandex.ru>
2024-02-27 18:05:29 +0300

    feat: add clients login logic

--- lab1/src/Banks/Bank.java
+++ lab1/src/Banks/Bank.java
@@ -6,10 +6,11 @@ import Clients.ClientBuilder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Bank {
-    private HashMap<Client, ArrayList<Account>> clients;
+    private final HashMap<Client, ArrayList<Account>> clients = new HashMap<>();
 
     public void addNewClient() {
         ClientBuilder clientBuilder = new ClientBuilder();
@@ -58,5 +59,32 @@ public class Bank {
         clients.put(clientBuilder.createClient(), new ArrayList<Account>());
 
         System.out.println("A new client was successfully added to the system");
+    }
+
+    public void loginClient() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("--LOGIN---");
+        System.out.println("Enter your name: ");
+
+        String name = scanner.nextLine();
+        System.out.println("Enter your surname: ");
+        String surname = scanner.nextLine();
+        int series;
+        int number = 0;
+
+
+        for (Map.Entry<Client, ArrayList<Account>> entry : clients.entrySet()) {
+            Client key = entry.getKey();
+            if (key.getName().equals(name) && key.getSurname().equals(surname)) {
+                if (key.getSeries() == 0 && key.getNumber() == 0) {
+                    break;
+                } else {
+                    System.out.println("Enter the series of your passport: ");
+                    series = scanner.nextInt();
+                    System.out.println("Enter the number of your passport: ");
+                    number = scanner.nextInt();
+                }
+            }
+        }
     }
 }
