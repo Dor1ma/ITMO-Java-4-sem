e3e1463082a729d337c3d7b18e89eeb33505cdf0
Pavel <admaikin.pavel@gmail.com>
2024-04-30 17:45:45 +0300

    feat: init commit

---
+++ lab3/src/main/java/com/example/lab3/Lab3Application.java
@@ -0,0 +1,12 @@
+package com.example.lab3;
+
+import org.core.models.Cat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Lab3Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3Application.class, args);
+    }
+
+}
