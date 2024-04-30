c2cbadadbe0e6d6abca1d47d85c4cc8b72f8e40d
Pavel <admaikin.pavel@gmail.com>
2024-04-30 21:25:36 +0300

    feat: add controllers

---
+++ lab3/src/main/java/controllers/OwnerController.java
@@ -0,0 +1,72 @@
+package controllers;
+
+import dto.OwnerDto;
+import org.springframework.http.HttpStatus;
+import services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/owners")
+public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OwnerDto> getOwner(@PathVariable Long id) {
+        try {
+            OwnerDto owner = ownerService.getOwner(id);
+            return ResponseEntity.ok(owner);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OwnerDto>> getAllOwners() {
+        try {
+            List<OwnerDto> owners = ownerService.getAllOwners();
+            return ResponseEntity.ok(owners);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<OwnerDto> createOwner(@RequestBody OwnerDto ownerDto) {
+        try {
+            OwnerDto createdOwner = ownerService.createOwner(ownerDto);
+            return ResponseEntity.ok(createdOwner);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OwnerDto> updateOwner(@PathVariable Long id, @RequestBody OwnerDto ownerDto) {
+        try {
+            OwnerDto updatedOwner = ownerService.updateOwner(id, ownerDto);
+            return ResponseEntity.ok(updatedOwner);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOwner(@PathVariable Long id) {
+        try {
+            ownerService.deleteOwner(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
