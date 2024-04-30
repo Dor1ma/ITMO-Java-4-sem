cfca011f85c598a7763f00164c874b6a367b44f9
Pavel <admaikin.pavel@gmail.com>
2024-04-30 19:47:16 +0300

    feat: create cat service, repository and wrapper class (dto)

---
+++ lab3/src/main/java/repository/CatRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.core.models.Cat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CatRepository extends JpaRepository<Cat, Long> {
+}
---
+++ lab3/src/main/java/repository/OwnerRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.core.models.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+}
